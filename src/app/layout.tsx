b25c2b1bed32089399d44b427ff8f39e7fa1ff74
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-05 11:40:33 -0300

    feat: add dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 
 import "@/styles/globals.css";
 
+import { ThemeProvider } from "@/providers/theme";
+
+import { Header } from "@/components/header";
+
 import { fonts } from "../../public/fonts";
 
 export const metadata: Metadata = {
@@ -15,7 +19,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
   const fontVariables = fonts.map((font) => font.variable).join(" ");
   return (
     <html lang="pt-br">
-      <body className={`${fontVariables} font-sans`}>{children}</body>
+      <body className={`${fontVariables} font-sans`}>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <>
+            <Header />
+            {children}
+          </>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
