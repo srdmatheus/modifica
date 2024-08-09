b26e54ea49c790a195d5a35f30b9f0c826381a08
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-09 18:46:58 -0300

    chore: config accepted files

---
+++ src/config/accepted-files.ts
@@ -0,0 +1,14 @@
+export const accepted_files = {
+  "image/*": [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".webp",
+    ".ico",
+    ".tif",
+    ".tiff",
+    ".tga"
+  ]
+};
