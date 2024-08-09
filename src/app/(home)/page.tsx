4c81626d8fc87647a08b939f5fd5f38a4cfb12de
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-09 18:51:03 -0300

    chore: change headline and texts

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,18 @@
-import { FileConverter } from "@/components/file-converter";
+import { FileHandler } from "@/components/conversion/file-handler";
 
 export default function Home() {
   return (
-    <main className="container mx-auto flex h-dvh flex-col items-center px-4">
-      <div className="flex w-2/3 flex-col gap-6 py-14">
-        <h1 className="text-center text-4xl font-medium tracking-tight">
-          Conversor de arquivos ilimitado <br /> e gratuito para sempre!! 🥳
+    <main className="container mx-auto flex flex-col items-center px-4">
+      <div className="flex max-w-[58ch] flex-col items-center gap-6 py-8 md:py-14">
+        <h1 className="text-center font-heading text-2xl font-extrabold tracking-tight md:text-4xl">
+          Conversor de imagens ilimitado e 100% gratuito para sempre!! 🥳
         </h1>
-        <p className="text-center text-foreground-muted">
-          A solução online perfeita para transformar e otimizar seus arquivos
-          multimídia de maneira rápida e gratuita. Converta e comprima imagens,
-          áudios e vídeos sem limitações, e dê vida nova ao seu conteúdo com
-          facilidade. Comece a usar o <strong>Modifica</strong> hoje e descubra
-          uma nova maneira de criar e compartilhar!
+        <p className="text-center text-sm text-foreground-muted md:text-base">
+          Solte, escolha o formato, converta! Tão simples quanto parece!
         </p>
       </div>
 
-      <FileConverter />
+      <FileHandler />
     </main>
   );
 }
