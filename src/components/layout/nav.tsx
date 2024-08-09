3f22efdf17a3c1d7417dd8f92f7672f4cd2dd9bc
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-09 18:46:22 -0300

    feat: add header component

---
+++ src/components/layout/nav.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from "react";
+
+import { navItems } from "@/config/site";
+import { cn } from "@/lib/utils";
+
+import { NavItem } from "./nav-item";
+
+type NavProps = ComponentProps<"nav">;
+
+export const Nav = ({ className, ...props }: NavProps) => {
+  return (
+    <nav className={cn("", className)} {...props}>
+      <ul className="flex items-center justify-center gap-2">
+        {navItems.map((item) => (
+          <NavItem key={item.href} {...item} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
