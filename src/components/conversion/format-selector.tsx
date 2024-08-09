b69c3c61fe325affa32eac577159370bcaacd364
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-09 18:49:44 -0300

    feat: add format selector component

---
+++ src/components/conversion/format-selector.tsx
@@ -0,0 +1,40 @@
+import { Select } from "../ui/select";
+
+type FormatSelectorProps = {
+  format: string;
+  onFormatChange: (format: string) => void;
+  disabled: boolean;
+};
+
+export const FormatSelector = (props: FormatSelectorProps) => {
+  const { format, onFormatChange, disabled } = props;
+
+  return (
+    <div className="flex items-center gap-2">
+      <span className="text-sm text-foreground-muted">Converter para:</span>
+      <Select.Root
+        value={format}
+        onValueChange={onFormatChange}
+        disabled={disabled}
+      >
+        <Select.Trigger className="w-[100px]">
+          <Select.Value placeholder="..." />
+        </Select.Trigger>
+        <Select.Content>
+          <Select.Group>
+            <Select.Item value="bmp">bmp</Select.Item>
+            <Select.Item value="gif">gif</Select.Item>
+            <Select.Item value="ico">ico</Select.Item>
+            <Select.Item value="jpeg">jpeg</Select.Item>
+            <Select.Item value="jpg">jpg</Select.Item>
+            <Select.Item value="png">png</Select.Item>
+            <Select.Item value="tga">tga</Select.Item>
+            <Select.Item value="tif">tif</Select.Item>
+            <Select.Item value="tiff">tiff</Select.Item>
+            <Select.Item value="webp">webp</Select.Item>
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+    </div>
+  );
+};
