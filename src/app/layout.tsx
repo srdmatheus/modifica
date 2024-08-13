a2fe277cdd98f056e795d6b134a70654bdb6cc41
Matheus Ribeiro <srdmatheus@gmail.com>
2024-08-12 23:56:57 -0300

    feat: add pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,51 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import "@/styles/globals.css";
 
 import { ThemeProvider } from "@/providers/theme";
 
+import { APP_CONFIG } from "@/config/site";
 import { Header } from "@/components/layout/header";
 
 import { fonts } from "../../public/fonts";
 
 export const metadata: Metadata = {
-  title: "Modifica | Converta suas imagens para diversos formatos",
-  description:
-    "Converta facilmente suas imagens para vários formatos como PNG, JPEG, GIF e outros com nosso conversor de imagens online. Rápido e simples.",
-  keywords:
-    "conversor de imagens, converter imagens, conversor de imagens online, converter para png, converter para jpeg, converter para gif, conversor de arquivos",
-  applicationName: "Modifica",
-  authors: [{ name: "Matheus Ribeiro", url: "https://rmatheus.com" }],
-  creator: "Matheus Ribeiro",
-  publisher: "Matheus Ribeiro",
-  colorScheme: "light dark",
-  icons: {
-    icon: "/favicon.ico"
+  applicationName: APP_CONFIG.APP_NAME,
+  title: {
+    default: APP_CONFIG.APP_DEFAULT_TITLE,
+    template: APP_CONFIG.APP_TITLE_TEMPLATE
   },
-  robots: {
-    index: true,
-    follow: true,
-    noarchive: false,
-    nocache: false
+  description: APP_CONFIG.APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_CONFIG.APP_DEFAULT_TITLE
+  },
+  formatDetection: {
+    telephone: false
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_CONFIG.APP_NAME,
+    title: {
+      default: APP_CONFIG.APP_DEFAULT_TITLE,
+      template: APP_CONFIG.APP_TITLE_TEMPLATE
+    },
+    description: APP_CONFIG.APP_DESCRIPTION
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_CONFIG.APP_DEFAULT_TITLE,
+      template: APP_CONFIG.APP_TITLE_TEMPLATE
+    },
+    description: APP_CONFIG.APP_DESCRIPTION
   }
+};
+
+export const viewport: Viewport = {
+  themeColor: "#fafafa"
 };
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
